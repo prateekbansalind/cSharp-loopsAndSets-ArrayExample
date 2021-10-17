501af67097428953d7edb642e92098bd97b1a6f7
pbansal16 <prateekbansalind@gmail.com>
2021-10-17 21:04:55 +1300

    Initial Commit : Array Exwrcise

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create an array of 3 names.
+            //Ask the user which number to select.When the user gives you a number, return that name.
+
+            string[] firstNames = new string[] { "Harry", "Justin", "Prince" };
+
+            Console.Write("Please type a number in between 0 and 2 to get a name of a person: ");
+            string xText = Console.ReadLine();
+
+            int x = int.Parse(xText);
+
+                
+
+
+            Console.WriteLine($"You have typed number {x} : {firstNames[x]}");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
